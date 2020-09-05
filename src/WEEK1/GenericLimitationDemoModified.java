cf263d3fe08abacc581eeea2a917bf4763497d86
Debojyoti Sinha <sinha.debojyoti75@gmail.com>
2020-09-06 01:02:45 +0530

    Create GenericLimitationDemoModified.java

---
+++ src/WEEK1/GenericLimitationDemoModified.java
@@ -0,0 +1,66 @@
+package WEEK1;
+
+class Student3<T extends Number> {
+    String name;
+    T[] marks;
+
+    Student3(T[] m) {
+        marks = m;
+    }
+
+    double total() {
+        double sum = 0.0;
+
+        for (int i = 0; i < marks.length; i++) {
+            sum += marks[i].doubleValue();
+        }
+
+        return (sum);
+    }
+
+    boolean compareMarks(Student3<?> others) {
+        if (total() == others.total()) {
+            return true;
+        }
+
+        return false;
+    }
+}
+
+public class GenericLimitationDemoModified {
+    public static void main(String[] args) {
+        Integer[] intMarks1 = {44, 55, 33, 66, 77};
+        Student3<Integer> s1IntMarks = new Student3<Integer>(intMarks1);
+        System.out.println("Total marks " + s1IntMarks.total());
+
+        Integer[] intMarks2 = {49, 39, 53, 69};
+        Student3<Integer> s2IntMarks = new Student3<Integer>(intMarks2);
+        System.out.println("Total marks " + s2IntMarks.total());
+
+        if (s1IntMarks.compareMarks(s2IntMarks)) {
+            System.out.println("Same marks.");
+        } else {
+            System.out.println("Different marks.");
+        }
+
+        Double[] doubleMarks = {43.5, 55.5, 32.5, 66.5, 77.0};
+        Student3<Double> s3DoubleMarks = new Student3<Double>(doubleMarks);
+        System.out.println("Total marks " + s3DoubleMarks);
+
+        Float[] floatMarks = {50.0F, 40.0F, 60.0F, 65.0F};
+        Student3<Float> s4FloatMarks = new Student3<Float>(floatMarks);
+        System.out.println("Total marks " + s4FloatMarks);
+
+        if (s2IntMarks.compareMarks(s3DoubleMarks)) {
+            System.out.println("Same marks.");
+        } else {
+            System.out.println("Different marks.");
+        }
+
+        if (s3DoubleMarks.compareMarks(s4FloatMarks)) {
+            System.out.println("Same marks.");
+        } else {
+            System.out.println("Different marks.");
+        }
+    }
+}
