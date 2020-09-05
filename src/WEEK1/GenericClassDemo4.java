8e2659f4a54b7299bf64255bd2f6e6dab752a114
Debojyoti Sinha <sinha.debojyoti75@gmail.com>
2020-09-05 22:46:46 +0530

    Create GenericClassDemo4.java

---
+++ src/WEEK1/GenericClassDemo4.java
@@ -0,0 +1,14 @@
+package WEEK1;
+
+class GenericClass4<T, V> {
+    T obj1;
+    V obj2;
+
+    GenericClass4(T obj1, V obj2) {
+        this.obj1 = obj1;
+        this.obj2 = obj2;
+    }
+}
+
+public class GenericClassDemo4 {
+}
