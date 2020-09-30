9438bf0c767547526204c56ec99e1103b08752ac
Debojyoti Sinha <sinha.debojyoti75@gmail.com>
2020-09-30 20:49:41 +0530

    test

--- src/WEEK1/DemoClass.java
+++ src/WEEK1/DemoClass.java
@@ -8,7 +8,7 @@ public class DemoClass {
     public static void main(String[] args) {
         DemoClass aObj = new DemoClass();
 
-        aObj.genericPrint(10123);
+        aObj.genericPrint(10178);
         aObj.genericPrint("Joy with Java ");
         aObj.genericPrint(3.1412343234);
     }
