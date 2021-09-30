f9756b3bbb9c8e414423ab84b4f98b2a9e20037a
Debojyoti Sinha <sinha.debojyoti75@gmail.com>
2021-09-30 20:56:16 +0530

    Update DemoClass.java

--- src/WEEK1/DemoClass.java
+++ src/WEEK1/DemoClass.java
@@ -2,14 +2,17 @@ package WEEK1;
 
 public class DemoClass {
     <T> void genericPrint(T t) {
-        System.out.println(t);
+        for(int i = 0; i < 10; i++)
+        {
+            System.out.println(t);
+        }
     }
 
     public static void main(String[] args) {
         DemoClass aObj = new DemoClass();
 
         aObj.genericPrint(10178);
-        aObj.genericPrint("Joy with Java ");
+        aObj.genericPrint("Joy with Java \");
         aObj.genericPrint(3.1412343234);
     }
 }
