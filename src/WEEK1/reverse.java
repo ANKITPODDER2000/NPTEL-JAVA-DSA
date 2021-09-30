0f613fece84bb01b8c110f0ecb3f5bd5fb12090b
Debojyoti Sinha <sinha.debojyoti75@gmail.com>
2021-09-30 21:15:36 +0530

    Create reverse.java

---
+++ src/WEEK1/reverse.java
@@ -0,0 +1,28 @@
+package WEEK1;
+
+class SpecificArrayInt {
+    int[] a;
+
+    SpecificArrayInt(int[] a) {
+        this.a = a;
+    }
+
+    void reverseInt() {
+        int j = a.length - 1;
+
+        for (int i = 0; i < j; i++) {
+            int temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+        }
+    }
+}
+
+public class MainClassInt {
+    public static void main(String[] args) {
+        SpecificArrayInt a = new SpecificArrayInt(new int[]{1, 2, 3, 4, 5});
+
+        a.printInt();
+        a.reverseInt();
+    }
+}
